cd470a914908d86326fffff33c1c3aaae616d312
antirez <antirez@gmail.com>
2016-04-25 10:39:02 +0200

    Modules: a few fixes for the zset iterator.

--- src/modules/helloworld.c
+++ src/modules/helloworld.c
@@ -383,7 +383,7 @@ int HelloZsumRange_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, i
  * The command will return all the sorted set items that are lexicographically
  * between the specified range (using the same format as ZRANGEBYLEX)
  * and having an age between min_age and max_age. */
-int HelloZsumRange_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+int HelloLexRange_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
     RedisModule_AutoMemory(ctx); /* Use automatic memory management. */
 
     if (argc != 6) return RedisModule_WrongArity(ctx);
@@ -394,9 +394,12 @@ int HelloZsumRange_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, i
         return RedisModule_ReplyWithError(ctx,REDISMODULE_ERRORMSG_WRONGTYPE);
     }
 
-    RedisModule_ReplyWithArray(ctx,REDISMODULE_POSTPONED_ARRAY_LEN);
-    RedisModule_ZsetFirstInLexRange(key,argv[2],argv[3]);
+    if (RedisModule_ZsetFirstInLexRange(key,argv[2],argv[3]) != REDISMODULE_OK) {
+        return RedisModule_ReplyWithError(ctx,"invalid range");
+    }
+
     int arraylen = 0;
+    RedisModule_ReplyWithArray(ctx,REDISMODULE_POSTPONED_ARRAY_LEN);
     while(!RedisModule_ZsetRangeEndReached(key)) {
         double score;
         RedisModuleString *ele = RedisModule_ZsetRangeCurrentElement(key,&score);
@@ -406,7 +409,7 @@ int HelloZsumRange_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, i
         arraylen++;
     }
     RedisModule_ZsetRangeStop(key);
-    RedisModule_SetArrayLength(ctx,arraylen);
+    RedisModule_ReplySetArrayLength(ctx,arraylen);
     RedisModule_CloseKey(key);
     return REDISMODULE_OK;
 }
@@ -467,6 +470,10 @@ int RedisModule_OnLoad(RedisModuleCtx *ctx) {
 
     if (RedisModule_CreateCommand(ctx,"hello.zsumrange",
         HelloZsumRange_RedisCommand) == REDISMODULE_ERR)
+        return REDISMODULE_ERR;
+
+    if (RedisModule_CreateCommand(ctx,"hello.lexrange",
+        HelloLexRange_RedisCommand) == REDISMODULE_ERR)
         return REDISMODULE_ERR;
 
     return REDISMODULE_OK;
